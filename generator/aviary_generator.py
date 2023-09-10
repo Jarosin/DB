0054f619d7ef598f9dc999b3519f3a01aa9c3877
Spirin Mihail <misha_spirin_90@mail.ru>
2023-09-11 00:32:16 +0300

    add animal and aviary generation

--- generator/aviary_generator.py
+++ generator/aviary_generator.py
@@ -1,7 +1,7 @@
 from base_generator import BaseGenerator
 import random
 
-class AnimalGenerator(BaseGenerator):
+class AviaryGenerator(BaseGenerator):
     def __init__(self) -> None:
         super().__init__()
         self.names = ["wolve", "lion", "antelope", "beaver", "capybara",
@@ -12,29 +12,30 @@ class AnimalGenerator(BaseGenerator):
                       "buffalo", "bull", "kangaroo", "bear", "camel", "rabbit",
                       "donkey", "deer", "tiger", "pig", "giraffe", "turtle"]
 
-        self.weight_max = 2000
-        self.weight_min = 0.01
+        self.pavilion_types = ('asia', 'europe', 'north america', 'south america', 'australia')
 
-        self.height_min = 10
-        self.height_max = 320
+        self.size_min = 600
+        self.size_max = 5000
 
-        self.age_min = 1
-        self.age_max = 100
+        self.dates = [1936, 1954, 1969, 1990, 2008, 2021]
+
+        self.stuff_per_hundred_square_meter = 0.2
+
 
     def fill_db(self, db_connection, amount_of_records):
         cursor = db_connection.cursor()
-        insert_query = """INSERT INTO animals (id, aviary_id, name, weight, height, endangered, age)"""
+        insert_query = """INSERT INTO aviary (id, size, pavilion, is_outdoors, construction_date, cleaning_stuff_size)"""
 
         for i in range(amount_of_records):
-            weight = random.uniform(self.weight_min, self.weight_max)
-            height = weight / 0.45
+            size = random.randint(self.size_min, self.size_max)
+            stuff_size = size / 100 * 0.2
             record_to_insert = (random.randint(0, amount_of_records - 1),
-                                random.randint(0, amount_of_records - 1),
-                                self.names[random.randint(0, len(self.names) - 1)],
-                                weight,
-                                height,
+                                size,
+                                self.pavilion_types[random.randint(0, len(self.names) - 1)],
                                 bool(random.random()),
-                                random.randint(self.age_min, self.age_max))
+                                self.dates[random.randint(0, len(self.dates) - 1)],
+                                stuff_size
+                                )
 
             cursor.execute(insert_query, record_to_insert)
         return
